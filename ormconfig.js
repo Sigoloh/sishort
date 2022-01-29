74306dd717cda6b4b06c687c611ab7a778a3c684
Augusto Sigolo <augusto@sigolo.me>
2022-01-29 16:38:47 -0300

    :ambulance: Mudado o nome da variavel de url do banco de dados para um que o heroku aceite

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
    "type": "mongodb",
-   "url": process.env.DATABASE_URL,
+   "url": process.env.DATABASE_CONNECTION_URL,
    "synchronize": true,
    "logging": false,
    "entities": [
